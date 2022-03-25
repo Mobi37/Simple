4bcd5b3362f179782ca992d671857f666591e58f
Mobi37 <61323278+Mobi37@users.noreply.github.com>
2022-03-25 16:31:17 +0500

    Task(25.03)

---
+++ Javascript/11.8/task 2/main.js
@@ -0,0 +1,35 @@
+class HttpError extends Error {
+    constructor(response) {
+      super(`${response.status} for ${response.url}`);
+      this.name = 'HttpError';
+      this.response = response;
+    }
+}
+  
+async function loadJson(url) {
+    let response = await fetch(url);
+    if (response.status == 200) {
+        return await response.json();
+    }
+    throw new HttpError(response);
+}
+  
+async function demoGithubUser() {
+    let name = prompt("Введите логин?", "iliakan");
+
+    try{
+        let user = await loadJson(`https://api.github.com/users/${name}`)
+        alert(`Полное имя: ${user.name}.`);
+        return user;
+    }catch(err){
+        if (err instanceof HttpError && err.response.status == 404) {
+          alert("Такого пользователя не существует, пожалуйста, повторите ввод.");
+          return demoGithubUser();
+        } else {
+          throw err;
+        }
+    }
+
+}
+  
+demoGithubUser();
