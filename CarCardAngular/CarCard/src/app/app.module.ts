4685c4ff43ee4ad9900d0a31316625b50d4be017
Mobi37 <61323278+Mobi37@users.noreply.github.com>
2022-05-05 15:04:58 +0500

    Update app.module.ts

--- CarCardAngular/CarCard/src/app/app.module.ts
+++ CarCardAngular/CarCard/src/app/app.module.ts
@@ -1,22 +1,15 @@
 import { NgModule } from '@angular/core';
-import { BrowserModule } from '@angular/platform-browser';
-import { HttpClientModule } from '@angular/common/http';
-
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { TableCarsModule } from './table-cars/table-cars.module';
-import { CommonModule } from '@angular/common';
 
 @NgModule({
   declarations: [
     AppComponent,
   ],
   imports: [
-    CommonModule,
-    BrowserModule,
     AppRoutingModule,
-    HttpClientModule ,
     TableCarsModule
   ],
   providers: [  ],
